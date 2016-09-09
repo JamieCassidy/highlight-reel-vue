48ae6a0c7afe9e473e841f62b0c874cb4d63d641
Jamie Cassidy <jamiec90@gmail.com>
2016-09-09 15:48:37 +0100

    Initial commit

---
+++ src/main.js
@@ -0,0 +1,53 @@
+var css = require("./scss/application.scss");
+var Vue = require('vue');
+
+
+Vue.component('highlight-reel', {
+  template: '#highlight-reel',
+  props: ['dribbbleUsername'],
+
+  data: function() {
+    return {
+      shots: []
+    }
+  },
+
+  created: function() {
+    var requestURL = 'https://api.dribbble.com/v1/users/' + this.dribbbleUsername + '/shots?access_token=75c7d4e3de51b464f3eedee1e2e8ce0a7a4d9becac21696d675ee57c35d14098';
+    this.requestShots(requestURL);
+  },
+
+  methods: {
+    requestShots: function(requestURL) {
+      var self = this;
+      this.getJSON(requestURL, function(response) {
+        self.getShots(response);
+      })
+    },
+
+    getJSON: function(url, callback) {
+      request = new XMLHttpRequest();
+      request.open('GET', url, true);
+      request.onload = function() {
+        if(request.status >= 200 && request.status < 400) {
+          response = JSON.parse(request.responseText);
+        } else {
+          response = request.status;
+        }
+
+        return callback(response);
+      }
+
+      request.send();
+    },
+
+    getShots: function(response) {
+      this.shots = response;
+      console.log(response);
+    }
+  }
+});
+
+new Vue({
+  el: '#vue-dribbble'
+});
---
+++ webpack.config.js
@@ -0,0 +1,20 @@
+module.exports = {
+  entry: "./src/main.js",
+  output: {
+    path: "./build",
+    publicPath: "/build/",
+    filename: "build.js"
+  },
+  module: {
+    loaders: [
+      {
+        test: /\.scss$/,
+        loaders: ["style", "css", "sass"]
+      },
+      {
+        test: /\.html$/,
+        loader: "html"
+      }
+    ]
+  }
+}
